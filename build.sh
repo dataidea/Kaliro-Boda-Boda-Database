bcacfa1813292daec67836298b4c9e3d0aadb094
jumashafara <jumashafara0@gmail.com>
2023-01-23 18:05:41 +0300

    added create superuser command

--- build.sh
+++ build.sh
@@ -11,3 +11,7 @@ python manage.py makemigrations
 python manage.py migrate
 
 pip install --force-reinstall -U setuptools
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
